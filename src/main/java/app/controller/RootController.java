c28980b3172fc6a05ca8eda7eaedc1dfaba3810d
WhatIsAPanda <arobinson4224@gmail.com>
2025-11-09 15:02:34 +0100

    explore files

--- src/main/java/app/controller/RootController.java
+++ src/main/java/app/controller/RootController.java
@@ -29,7 +29,7 @@ public class RootController {
         WORKSPACE_PAGE, "/app/Workspace.fxml",
         "map", "/app/Map.fxml",
         "gallery", "/app/Gallery.fxml",
-        "login", "/app/LoginpagePretty.fxml"
+        "login", "/app/Login.fxml"
     );
 
     public RootController() {
