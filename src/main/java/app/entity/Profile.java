e61d12fdfd13396b0005cf057d215bb48e17e2ce
WhatIsAPanda <arobinson4224@gmail.com>
2025-11-17 17:22:32 +0100

    3

--- src/main/java/app/entity/Profile.java
+++ src/main/java/app/entity/Profile.java
@@ -8,16 +8,17 @@ import java.util.List;
 public class Profile {
     private int account_id;
     private String username;
-    private String biography;
+    public String biography;
     private String profile_picture_url;
-    private Image profile_picture;
+    public Image profile_picture;
     private String work_address;
-    private double work_longitude;
-    private double work_latitude;
+    public double work_longitude;
+    public double work_latitude;
     private List<String> styles_list;
     private List<Post> artistPosts;
 
-    public Profile(int account_id,  String username, String profile_picture_url, String biography, String work_address, double work_longitude, double work_latitude, List<String> styles_list) {
+    public Profile(int account_id, String username, String profile_picture_url, String biography, String work_address,
+            double work_longitude, double work_latitude, List<String> styles_list) {
         this.account_id = account_id;
         this.username = username;
         this.biography = biography;
@@ -37,10 +38,6 @@ public class Profile {
         return username;
     }
 
-    public String getBiography() {
-        return biography;
-    }
-
     public String getProfilePictureURL() {
         return profile_picture_url;
     }
@@ -49,32 +46,21 @@ public class Profile {
         return work_address;
     }
 
-    public double getLongitude() {
-        return work_longitude;
-    }
-
-    public double getLatitude() {
-        return work_latitude;
-    }
-
     public List<String> getStylesList() {
         return styles_list;
     }
-    public Image getProfilePicture() {
-        return profile_picture;
-    }
+
     public void setArtistPosts(List<Post> artistPosts) {
 
         this.artistPosts = artistPosts;
     }
+
     public List<Post> getArtistPosts() {
         return artistPosts != null ? artistPosts : Collections.emptyList();
     }
 
     // TEMP shim to satisfy legacy probe tests (DbProfilesProbeTest etc.).
-// TODO(Adnan): remove once probe tests are updated to new data model.
+    // TODO(Adnan): remove once probe tests are updated to new data model.
     // public java.util.List<app.entity.Post> getPosts() { return null; }
-
-
 
 }
