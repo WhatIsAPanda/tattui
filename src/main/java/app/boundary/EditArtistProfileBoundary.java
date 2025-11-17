dec18ac9532b424a8012d8f558752f3eceb6173c
SeviJack <167508118+SeviJack@users.noreply.github.com>
2025-11-17 17:07:00 +0100

    fixed sime stuff

--- src/main/java/app/boundary/EditArtistProfileBoundary.java
+++ src/main/java/app/boundary/EditArtistProfileBoundary.java
@@ -4,16 +4,17 @@ import app.entity.DatabaseConnector;
 import app.entity.Profile;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.input.MouseEvent;
 import java.sql.SQLException;
 
 public class EditArtistProfileBoundary extends BaseProfileBoundary {
 
     @FXML private Circle profilePicture;
     @FXML private TextArea biographyField;
-    @FXML private TextField artistNameField;
+    @FXML private Label artistNameField;
     @FXML private GridPane postsPanel;
 
     private Profile profile;
@@ -27,7 +28,19 @@ public class EditArtistProfileBoundary extends BaseProfileBoundary {
             e.printStackTrace();
         }
     }
+    @FXML
+    private void handleBack() {
+        System.out.println("Back clicked");
+    }
 
+    @FXML
+    private void handleProfileClick(javafx.scene.input.MouseEvent event) {
+        System.out.println("Profile clicked");
+    // TODO: your desired behavior (open profile edit, etc.)
+    @FXML 
+    private void handleSaveChanges(javafx.event.ActionEvent event) {
+        System.out.println("Save Changes clicked");
+    }
     private void loadProfile() {
         populateProfileCommon(profile, profilePicture, biographyField, artistNameField);
         populatePosts(postsPanel, profile.getArtistPosts());
